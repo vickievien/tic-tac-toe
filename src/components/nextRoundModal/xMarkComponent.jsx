33ee95492c9125a9c80c3af56860d24dac580601
Vickie Vien <vickievien@gmail.com>
2022-10-18 12:12:06 -0700

    chore: copying code over to new repo

---
+++ src/components/nextRoundModal/xMarkComponent.jsx
@@ -0,0 +1,12 @@
+const Xmark = () => {
+    return (
+        <>
+            <p className="subtitle">oh, looks like...</p>
+            <div className="message-container">
+                <img src="/assets/icon-x.svg" alt="x-mark"/>
+                <p className="light-blue">takes the round</p>
+            </div>
+        </>
+    )
+}
+export default Xmark;
