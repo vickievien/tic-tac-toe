c648cac18b6fdc3e438928beed23e34acf202ab3
Vickie Vien <vickievien@gmail.com>
2022-10-18 12:11:33 -0700

    chore: copying code over to new repo

---
+++ src/components/nextRoundModal/oMarkComponent.jsx
@@ -0,0 +1,12 @@
+const Omark = () => {
+    return (
+        <>
+            <p className="subtitle">oh, looks like...</p>
+            <div className="message-container">
+                <img src="/assets/icon-o.svg" alt="o-mark"/>
+                <p className="light-yellow">takes the round</p>
+            </div>
+        </>
+    )
+}
+export default Omark;
