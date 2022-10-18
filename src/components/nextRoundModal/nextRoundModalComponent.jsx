424241484681d966d4ff67a9ae9619a7ca855c3a
Vickie Vien <vickievien@gmail.com>
2022-10-18 12:11:08 -0700

    chore: copying code over to new repo

---
+++ src/components/nextRoundModal/nextRoundModalComponent.jsx
@@ -0,0 +1,22 @@
+import React, { useState } from "react"
+import NewGameMenuComponent from "../newGame/newGameMenuComponent"
+import Omark from "./oMarkComponent"
+import Xmark from "./xMarkComponent"
+import Tiemark from "./tieMarkComponent"
+const NextRoundModalComponent = (props) => {
+    const toggleQuitGame = () => {
+        props.setNewGameModal(true);
+    }
+    return (
+            <div className="modal-full-container">
+            <section className="modal-container">
+                {props.winnerDisplay === 'x' ? <Xmark /> : props.winnerDisplay === 'o' ? <Omark /> : props.winnerDisplay === 'tie' ? <Tiemark /> :""}
+                <div>
+                    <a href='#' className='button button-silver button-modal' onClick={toggleQuitGame}>Quit</a>
+                    <a href='#' className='button button-yellow button-modal'onClick={props.clearGrid}>Next Round</a>
+                </div>
+            </section>
+            </div>
+    )
+}
+export default NextRoundModalComponent;
