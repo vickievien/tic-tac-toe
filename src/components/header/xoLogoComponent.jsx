84827ad695bc45204ad0dc22e52a5ced00b625d4
Vickie Vien <vickievien@gmail.com>
2022-10-18 12:09:40 -0700

    chore: copying code over to new repo

---
+++ src/components/header/xoLogoComponent.jsx
@@ -0,0 +1,9 @@
+const XoLogoComponent = () => {
+    return (
+        <div className='xo-logo-container'>
+            <img src="/assets/icon-x.svg" alt="x-mark"/>
+            <img src="/assets/icon-o.svg" alt="o-mark"/>
+        </div>
+    )
+}
+export default XoLogoComponent;
